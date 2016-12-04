e42af374a60dbdc339a9470c73545a3f2a565d7c
unknown <>
2016-12-04 15:54:31 -0500

    login

--- chat/auth0-variables.js
+++
@@ -1,3 +0,0 @@
-var AUTH0_CLIENT_ID='9gNiI2Sndqqz2sh3jfCaNmSx2hLsxmZl';
-var AUTH0_DOMAIN='dsaq.auth0.com';
-var AUTH0_CALLBACK_URL=location.href;
--- login/routes/index.js
+++ login/routes/index.js
@@ -3,9 +3,9 @@ var passport = require('passport');
 var router = express.Router();
 
 var env = {
-  AUTH0_CLIENT_ID: '9gNiI2Sndqqz2sh3jfCaNmSx2hLsxmZl',
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CALLBACK_URL: 'http://127.0.0.1:8888/'
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
 };
 
 /* GET home page. */
