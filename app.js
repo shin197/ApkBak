c311608485c07657dc78817fd9f49d9ac5f7c3cb
shin197 <shin197@purdue.edu>
2016-12-05 05:38:33 +0900

    works with chat server

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
+
+/*
 var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
@@ -14,6 +16,7 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+var http = re
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -25,4 +28,55 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});*/
+
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require("express");
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require("cfenv");
+
+// create a new express server
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
 });
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  }); 
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + "/public"));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+server.listen(appEnv.port, "0.0.0.0", function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
+
