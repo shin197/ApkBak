ef95171d89b32777349d1574e164b64900ded60d
shin197 <shin197@purdue.edu>
2016-12-05 23:14:19 +0900

    added bad day

--- app.js
+++ app.js
@@ -1,33 +1,85 @@
 
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+/*
 var express = require("express");
-//var cfenv = require("cfenv");
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require("cfenv");
+
+// create a new express server
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  }); 
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + "/public"));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+server.listen(appEnv.port, "0.0.0.0", function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
+*/
+
+var express = require("express");
+var cfenv = require("cfenv");
 var app = express();
 //var serv = require('http').Server(app);
 var http = require('http');
 var serv = http.createServer(app);
 var io = require('socket.io').listen(serv);
-var users = [];
 //var io = require('socket.io')(serv,{});
 
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use('/client',express.static(__dirname + '/public'));
+var appEnv = cfenv.getAppEnv();
+serv.listen(appEnv.port, "0.0.0.0", function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
+//serv.listen(1160);
+//console.log("Server started.");
 var SOCKET_LIST = {};
 var PLAYER_LIST = {};
 var uid = 0;
 function XOR(a,b) {
   return ( a || b ) && !( a && b );
 }
-var WIDTH = 1000;
+var WIDTH = 1600;
 var HEIGHT = 800;
-
-io.emit('some event', { for: 'everyone' });
-
-serv.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
 var intersect = function(ax,ay,aw,ah,bx,by,bw,bh){
 	return ax < bx+bw && ay < by+bh && bx < ax+aw && by < ay+ah;
 };
@@ -53,6 +105,7 @@ var Player = function(id){
 			if(this.msgExpireTimer == 0){
 				this.msg = null;
 			}
+			
 			this.xspeed = 0;
 			this.yspeed = 0;
 			if(XOR(this.pressingLeft,this.pressingRight))
@@ -79,61 +132,14 @@ var Player = function(id){
 				this.y = 0;
 			if(this.y + this.size > HEIGHT)
 				this.y = HEIGHT - this.size;
+			
 		}
 	}
 	return self;
 }
 
-io.sockets.on('connection', function(socket){
-  socket.on('new user', function(data, callback) {
-    if(data in users) {
-      callback(false);
-    } else {
-      callback(true);
-      socket.nickname = data;
-      console.log('a user connected');
-      callback(socket.nickname + ' is connected!');
-      io.sockets.emit('new message', {msg: socket.nickname + ' is connected', nick: 'Information' });
-      users[socket.nickname] = socket;
-      updateNicknames();
-    }
-  });
-  function updateNicknames() {
-    io.sockets.emit('usernames', Object.keys(users));
-  }
-
-  socket.on('send message', function(data, callback) {
-  	var msg = data.trim();
-  	if(msg.substr(0,3) == '/w ') {
-  		msg = msg.substr(3);
-  		var ind = msg.indexOf(' ');
-  		if(ind != -1) {
-  			var name = msg.substr(0, ind);
-  			var msg = msg.substr(ind + 1);
-  			if(name in users) {
-  				users[name].emit('whisper', {msg: msg, nick: socket.nickname});
-  				//console.log('Wisper: ');
-  			} else {
-  				callback('Error: Enter a valid user.');
-  			}
-  		} else {
-  			callback('Error: Please enter a message for your whisper.');
-  		}
-  	} else {
-    	io.sockets.emit('new message', {msg: data, nick: socket.nickname});
-  	}
-  });
-
-  socket.on('disconnect', function(data) {
-    if(!socket.nickname) return;
-    //console.log('a user disconnected');
-    callback(socket.nickname + ' is disconnected!');
-    io.sockets.emit('new message', {msg: socket.nickname + ' is disconnected', nick: 'Information'});
-    delete users[socket.nickname];
-    updateNicknames();
-  })
-
-  socket.id = uid++;
+io.sockets.on('connection',function(socket){
+	socket.id = uid++;
 	SOCKET_LIST[socket.id] = socket;
 	var player = Player(socket.id);
 	PLAYER_LIST[player.id] = player;
@@ -151,9 +157,7 @@ io.sockets.on('connection', function(socket){
 		else if(data.inputId === 'down')
 			player.pressingDown = data.state;
 	});
-
 });
-
 setInterval(function(){
 	var pack = [];
 	for(var i in PLAYER_LIST){
@@ -170,4 +174,5 @@ setInterval(function(){
 		var socket = SOCKET_LIST[i];
 		socket.emit('newPositions',pack);
 	}
+	
 },1000/60);
